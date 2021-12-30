b8ae1716b13c49b072d6d2bb3425d0bf1eb112d4
Andrew Hersh <hersh4@llnl.gov>
2021-12-29 16:48:01 -0800

    Initial commit.

--- bw2pass.py
+++ bw2pass.py
@@ -1,30 +1,84 @@
 #!/usr/bin/env python3
 
-import json
+import csv
 import argparse
 
+from subprocess import Popen, PIPE
+from collections import namedtuple
 
-def import_bw_dump(filename):
+_BWItem = namedtuple(
+    "_BWItem",
+    [
+        "folder",
+        "favorite",
+        "type",
+        "name",
+        "notes",
+        "fields",
+        "reprompt",
+        "login_uri",
+        "login_username",
+        "login_password",
+        "login_totp",
+    ],
+)
 
-    grouped_items = {}
 
-    with open(filename, "r") as json_file:
-        json_pass = json.load(json_file)
+class BWVault:
+    """
+    This class represents the whole bitwardent vault containing all the items
+    """
 
-    for item in json_pass["items"]:
-        name = item["name"]
-        if name in grouped_items:
-            pass
-        else:
-            grouped_items[name] = [item]
+    def __init__(self):
+        self.items = []
 
-    return grouped_items
+    def import_bw_dump(self, filename):
+
+        grouped_items = {}
+        item_count = 0
+        with open(filename, "r") as vault_file:
+            reader = csv.DictReader(vault_file)
+
+            # this is some dedupe logic, there is probably a better way
+            for row in reader:
+                name = row["name"]
+                if name in grouped_items:
+                    pass
+                else:
+                    grouped_items[name] = row["name"]
+                    self.items.append(
+                        _BWItem(
+                            row["folder"],
+                            row["favorite"],
+                            row["type"],
+                            row["name"],
+                            row["notes"],
+                            row["fields"],
+                            row["reprompt"],
+                            row["login_uri"],
+                            row["login_username"],
+                            row["login_password"],
+                            row["login_totp"],
+                        )
+                    )
+
+    def create_pass_entry(self, bw_item):
+
+        msg = f"{bw_item.login_password}\nUSERNAME: {bw_item.login_username}\n NOTES: {bw_item.notes}"
+        pass_process = Popen(
+            ["pass", "insert", "--multiline", bw_item.name], stdin=PIPE, stdout=PIPE
+        )
+        pass_process.communicate(msg.encode())
+        pass_process.wait()
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", type=str, help="json bw export file")
+    parser.add_argument("filename", type=str, help="bw export file")
     args = parser.parse_args()
 
-    print(import_bw_dump(args.filename))
+    vault = BWVault()
+    vault.import_bw_dump(args.filename)
+    for item in vault.items:
+        vault.create_pass_entry(item)
